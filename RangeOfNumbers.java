6a1ad4c08cdd2e72a72f47c3289700f4d8ddc00e
abysoll-hg <slickspam4u@gmail.com>
2025-11-17 09:21:23 -0800

    Added files

---
+++ RangeOfNumbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RangeOfNumbers {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.print("Start range: ");
+      int start = scan.nextInt();
+      System.out.print("End range: ");
+      int end = scan.nextInt();
+      if (end > start) {
+         for (int i = start; i < end; i++) {
+            System.out.print(i+", ");
+         }
+         System.out.print(end);
+      }
+      else if (start > end) {
+         for (int j = end; j < start; j++) {
+            System.out.print(j+", ");
+         }
+         System.out.print(start);   
+      }
+   }
+}
---
+++ SentinelLoop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SentinelLoop {
+   public static void main(String[] args) {
+      int total = 0;
+      Scanner scan = new Scanner(System.in);
+      System.out.print("Please enter a number or -1 to stop: ");
+      int n = scan.nextInt();
+      while (n != -1) {
+         System.out.println("You entered: " + n);
+         total += n;
+         System.out.print("Enter another number or -1 to stop: ");
+         n = scan.nextInt();
+      }
+      System.out.println(total);
+   }
+}
